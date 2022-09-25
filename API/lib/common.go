78ea2d9852dc3eafa393ecb626c8090e89c64255
ozraru <34647405+ozraru@users.noreply.github.com>
2022-09-25 16:32:10 +0900

    Change PasswordMode

--- API/lib/common.go
+++ API/lib/common.go
@@ -3,8 +3,8 @@ package lib
 type PasswordMode int
 
 const (
-	PasswordModeDynamic         PasswordMode = 0
-	PasswordModeStaticPlain     PasswordMode = 1
-	PasswordModeStaticEncrypted PasswordMode = 2
-	PasswordModeStaticUnstored  PasswordMode = 3
+	PasswordModeDynamic         PasswordMode = 1
+	PasswordModeStaticPlain     PasswordMode = 2
+	PasswordModeStaticEncrypted PasswordMode = 3
+	PasswordModeStaticUnstored  PasswordMode = 4
 )
